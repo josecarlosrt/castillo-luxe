2c328938b230bb2c13b26b8abae459d5748b8ef3
josecarlosrt <59973113+josecarlosrt@users.noreply.github.com>
2025-01-04 16:28:26 -0600

    default-config update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site : "https://numonu.github.io",
-  base : "/Luxury-Architect"
+  integrations: [tailwind()]
 });
